b0a4ee16d7b5b7f11b12cc31f91d25e9d9ca608a
TripNRaVeR <noerialexeew@live.nl>
2012-09-04 23:51:12 +0200

    patch: s5k6a1gx03: implement custom camera drivers

--- include/media/s5k6a1gx03.h
+++ include/media/s5k6a1gx03.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2011 NVIDIA Corporation.
+ * Copyright (C) 2012 TripNDroid Mobile Engineering.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -21,29 +22,54 @@
 
 #include <linux/ioctl.h>  /* For IOCTL macros */
 
-/* IOCTL to set the operating resolution of camera */
-#define S5K6A1G_IOCTL_SET_MODE			_IOW('o', 1, struct s5k6a1gx03_mode)
-#define S5K6A1G_IOCTL_SET_FRAME_LENGTH	_IOW('o', 2, __u32)
-#define S5K6A1G_IOCTL_SET_COARSE_TIME	_IOW('o', 3, __u32)
-#define S5K6A1G_IOCTL_SET_GAIN			_IOW('o', 4, __u16)
+#define S5K6A1GX03_IOCTL_SET_MODE			_IOW('o', 1, struct s5k6a1gx03_mode)
+#define S5K6A1GX03_IOCTL_SET_FRAME_LENGTH	_IOW('o', 2, __u32)
+#define S5K6A1GX03_IOCTL_SET_COARSE_TIME	_IOW('o', 3, __u32)
+#define S5K6A1GX03_IOCTL_SET_GAIN			_IOW('o', 4, __u16)
+#define S5K6A1GX03_IOCTL_GET_STATUS			_IOR('o', 5, __u8)
+#define S5K6A1GX03_IOCTL_SET_BINNING    	_IOW('o', 6, __u8)
+#define S5K6A1GX03_IOCTL_TEST_PATTERN		_IOW('o', 7, enum s5k6a1gx03_test_pattern)
+#define S5K6A1GX03_IOCTL_SET_GROUP_HOLD	    _IOW('o', 8, struct s5k6a1gx03_ae)
+#define S5K6A1GX03_IOCTL_SET_CAMERA_MODE	_IOW('o', 10, __u32)
+#define S5K6A1GX03_IOCTL_SYNC_SENSORS		_IOW('o', 11, __u32)
+#define S5K6A1GX03_IOCTL_GET_SENSORDATA		_IOR('o', 12, struct s5k6a1gx03_sensordata)
 
-#define S5K6A1G_IOCTL_GET_STATUS		_IOR('o', 5, __u8)
+/* S5K6A1GX03 registers */
+#define S5K6A1GX03_SRM_GRUP_ACCESS          (0x3212)
+#define S5K6A1GX03_ARRAY_CONTROL_01         (0x3621)
+#define S5K6A1GX03_ANALOG_CONTROL_D         (0x370D)
+#define S5K6A1GX03_TIMING_TC_REG_18         (0x3818)
+#define S5K6A1GX03_TIMING_CONTROL_HS_HIGH   (0x3800)
+#define S5K6A1GX03_TIMING_CONTROL_HS_LOW    (0x3801)
+#define S5K6A1GX03_TIMING_CONTROL_VS_HIGH   (0x3802)
+#define S5K6A1GX03_TIMING_CONTROL_VS_LOW    (0x3803)
+#define S5K6A1GX03_TIMING_HW_HIGH           (0x3804)
+#define S5K6A1GX03_TIMING_HW_LOW            (0x3805)
+#define S5K6A1GX03_TIMING_VH_HIGH           (0x3806)
+#define S5K6A1GX03_TIMING_VH_LOW            (0x3807)
+#define S5K6A1GX03_TIMING_TC_REG_18         (0x3818)
+#define S5K6A1GX03_TIMING_HREFST_MAN_HIGH   (0x3824)
+#define S5K6A1GX03_TIMING_HREFST_MAN_LOW    (0x3825)
+#define S5K6A1GX03_H_BINNING_BIT            (1 << 7)
+#define S5K6A1GX03_H_SUBSAMPLING_BIT        (1 << 6)
+#define S5K6A1GX03_V_BINNING_BIT            (1 << 6)
+#define S5K6A1GX03_V_SUBSAMPLING_BIT        (1 << 0)
+#define S5K6A1GX03_GROUP_HOLD_BIT		(1 << 7)
+#define S5K6A1GX03_GROUP_LAUNCH_BIT		(1 << 5)
+#define S5K6A1GX03_GROUP_HOLD_END_BIT	(1 << 4)
+#define S5K6A1GX03_GROUP_ID(id)		(id)
 
-#define S5K6A1G_IOCTL_SET_GROUP_HOLD		_IOW('o', 6, struct s5k6a1gx03_ae)
-#define S5K6A1G_IOCTL_TEST_PATTERN		_IOW('o', 7, enum s5k6a1gx03_test_pattern)
-
-/* IOCTL to set the operating mode of camera.
- * This can be either stereo , leftOnly or rightOnly */
-#define S5K6A1G_IOCTL_SET_CAMERA_MODE	_IOW('o', 10, __u32)
-#define S5K6A1G_IOCTL_SYNC_SENSORS		_IOW('o', 11, __u32)
-
-/*
 enum s5k6a1gx03_test_pattern {
 	TEST_PATTERN_NONE,
 	TEST_PATTERN_COLORBARS,
 	TEST_PATTERN_CHECKERBOARD
 };
-*/
+
+struct s5k6a1gx03_sensordata {
+    __u32 fuse_id_size;
+	__u8 fuse_id[16];
+};
+
 struct s5k6a1gx03_mode {
 	int xres;
 	int yres;
@@ -60,6 +86,7 @@ struct s5k6a1gx03_ae {
 	__s32 gain;
 	__u8 gain_enable;
 };
+
 #ifdef __KERNEL__
 struct s5k6a1gx03_platform_data {
 	int (*power_on)(void);
@@ -69,4 +96,3 @@ struct s5k6a1gx03_platform_data {
 #endif /* __KERNEL__ */
 
 #endif  /* __S5K6A1GX03_H__ */
-
